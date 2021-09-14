ad3bfda35cd0a459e83bda0c83b6476cf7c5e54c
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-09-14 18:14:18 -0500

    get listar usuario(s)/manejo de excepciones

---
+++ glamour-restapi/src/main/java/com/glamour/restapi/GlamourRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.glamour.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamourRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlamourRestapiApplication.class, args);
+	}
+
+}
---
+++ glamour-restapi/src/test/java/com/glamour/restapi/GlamourRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glamour.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamourRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
